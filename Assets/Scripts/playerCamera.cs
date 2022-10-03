0dd263e5f8e00121a63bd309363948a1681e2159
Richie Munoz <munoz.ricardo16@yahoo.com>
2022-10-03 14:19:43 -0700

    first commit for playerCamera; added camera controls as well as a reticle

--- Assets/Scripts/playerCamera.cs
+++ Assets/Scripts/playerCamera.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class playerCamera : MonoBehaviour
 {
+    [SerializeField] int hzntlSens;
+    [SerializeField] int vrtclSens;
+
+    [SerializeField] int lockVerticalMin;
+    [SerializeField] int lockVerticalMax;
+
+    [SerializeField] bool invert;
+
+    float xRotataion;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.visible = false; //makes the mouse cursor invisible 
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        //get input
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * hzntlSens;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * vrtclSens;
+
+
+        if(!invert)
+        {
+            xRotataion -= mouseY;
+        }
+        else
+        {
+            xRotataion += mouseY;
+        }
+
+        //clamp camera rotation
+        xRotataion = Mathf.Clamp(xRotataion, lockVerticalMin, lockVerticalMax);
+
+        //rotate the camera on the x-axis
+        transform.localRotation = Quaternion.Euler(xRotataion, 0, 0);
+
+        //rotates the player
+        transform.parent.Rotate(Vector3.up * mouseX);
     }
 }
