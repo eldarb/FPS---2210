671b10c3c6c88e0cfecaac645bf7562c7d458010
Kyzilien Pacheco <mdpacheco@student.fullsail.edu>
2022-10-08 14:15:41 -0400

    EnemyAI local to pull merge. Two enemyAI scripts were made

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour, IDamage
 {
-
     [SerializeField] int HP;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void takeDamage(int damage)
     {
         HP -= damage;
-        if(HP<=0)
+
+        if (HP <= 0)
+        {
             Destroy(gameObject);
+        }
     }
 }
