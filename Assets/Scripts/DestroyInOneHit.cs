cb5b7c9741296c04a73f1d4bb29b6343afe821bf
eldarb <eldar.bertsel@gmail.com>
2022-10-10 22:58:52 -0400

    updated prefabs so they have their parameters

---
+++ Assets/Scripts/DestroyInOneHit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyInOneHit : MonoBehaviour, IDamage
+{
+    public void takeDamage(int damage)
+    {
+        Destroy(gameObject);
+    }
+}
