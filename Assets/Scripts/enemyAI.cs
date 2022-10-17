294359f4dc24dc827cc880a9a66a74958a904eaf
Kyzilien Pacheco <mdpacheco@student.fullsail.edu>
2022-10-17 19:41:51 -0400

    Spawner Script added Spawners made as a prefab

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -26,7 +26,6 @@ public class enemyAI : MonoBehaviour, IDamage
     // Start is called before the first frame update
     void Start()
     {
-        gameManager.instance.enemyCount++;
         gameManager.instance.enemyCountText.text = gameManager.instance.enemyCount.ToString("F0");
     }
 
